fe329fd62ee95bc67d1301c3b11851dd26ddc275
Prachi24599 <prachi24599@gmail.com>
2023-04-18 00:42:37 +0530

    check for winner player

--- script.js
+++ script.js
@@ -25,7 +25,13 @@ function initGame() {
   boxes.forEach((box, index) => {
     box.innerText = "";
     boxes[index].style.pointerEvents = "all";
+    /*remove the background color -- It also works
+    box.classList.remove("win");*/
+
+    //initialize the box with css properties again (adding existing classes)
+    boxes[index].classList = `box box${index + 1}`;
   });
+
   newGameBtn.classList.remove("active");
   gameInfo.innerText = `Current Player - ${currentPlayer}`;
 }
@@ -38,7 +44,41 @@ function swapTurn() {
 }
 
 function checkGameOver() {
-  newGameBtn.classList.add("active");
+  let answer = "";
+
+  winningPositions.forEach((position) => {
+    //all 3 boxes should be non-empty and exactly same in value
+    if (
+      (gameGrid[position[0]] !== "" ||
+        gameGrid[position[1]] !== "" ||
+        gameGrid[position[2]] !== "") &&
+      gameGrid[position[0]] === gameGrid[position[1]] &&
+      gameGrid[position[1]] === gameGrid[position[2]]
+    ) {
+      //check If winner is X or Y
+      if (gameGrid[position[0]] === "X") answer = "X";
+      else answer = "O";
+
+      //disable pointer events for all boxes (as we have found the winner)
+      boxes.forEach((box) => {
+        box.style.pointerEvents = "none";
+      });
+
+      //now we have found who is winner in which boxes
+      boxes[position[0]].classList.add("win");
+      boxes[position[1]].classList.add("win");
+      boxes[position[2]].classList.add("win");
+    }
+  });
+
+  //If we have found a winner
+  if (answer !== "") {
+    //show thw winner in paragraph
+    gameInfo.innerText = `Winner Player - ${answer}`;
+    //activate newGame button
+    newGameBtn.classList.add("active");
+    return;
+  }
 }
 
 function handleClick(index) {
